17c65974a4f2dd08442a9cbb20ff2a5ab2755f55
Hercolys Moraes Araújo <hercolysm@gmail.com>
2017-03-21 17:32:07 -0300

    Update mysql.sql

--- mysql.sql
+++ mysql.sql
@@ -1,1 +1,27 @@
-SELECT * FROM table;
+SELECT * FROM tabela ORDER BY coluna asc (ou desc); /* ordena */
+SELECT * FROM tabela WHERE val <= 2016; /* maior ou igual */
+SELECT * FROM tabela WHERE val != (ou <>) 2016; /* diferente */
+SELECT * FROM tabela WHERE val BETWEEN 2014 and 2015; /* entre */
+SELECT * FROM tabela WHERE val IN (2014,2016); /* esteja em */
+SELECT * FROM tabela WHERE val > 35 and val2 <= 30; /* e */
+SELECT * FROM tabela WHERE val > 35 or val2 = 30; /* ou */
+SELECT * FROM tabela WHERE val LIKE '%busca%'; /* busca */
+SELECT * FROM tabela WHERE val NOT LIKE '%busca%'; /* negação */
+SELECT * FROM tabela WHERE val LIKE 'PH%P'; /* php, photoshop */
+SELECT * FROM tabela WHERE val LIKE 'PH%P_'; /* php3, php4 */
+SELECT distinct coluna FROM tabela; /* distintos */
+SELECT count(id) FROM tabela; /* conta */
+SELECT max(coluna) FROM tabela; /* retorna o maior */
+SELECT min(coluna) FROM tabela; /* retorna o menor */
+SELECT sum(coluna) FROM tabela; /* retorna a soma */
+SELECT avg(coluna) FROM tabela; /* retorna a media */
+SELECT * FROM tabela GROUP BY coluna; /* agrupa */
+SELECT * FROM tabela GROUP BY coluna HAVING coluna > 1999; /*  */
+SELECT * FROM tabela ORDER BY field (coluna,valor) desc; /* escolher qual valor vem primeiro */
+SELECT concat(coluna1,' - ',coluna2) FROM tabela; /* concatena */
+SELECT * FROM tabela LIMIT posicao , qnt; /* limita */
+SELECT * FROM tabela WHERE coluna = substring('string',pos,tam); /* corta uma string */
+SELECT sec_to_time(segundos) FROM tabela; /* converte em segundos */
+SELECT coalesce(null,2,3) FROM tabela; /* retorna primeiro resultado não nulo */  
+RENAME TABLE tabela TO novo_nome; /* renomeia tabela */
+SHOW COLUMNS FROM tabela; /* mostra colunas */
