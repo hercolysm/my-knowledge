511553b38d1b2efc629c290bbe5278836f0b27d9
Hercolys Moraes Araújo <hercolysm@gmail.com>
2017-10-10 00:07:42 -0300

    Update DDL - Data Definition Language.sql
    Adicionado comandos CREATE e DROP database

--- Banco de Dados/MySQL/DDL - Data Definition Language.sql
+++ Banco de Dados/MySQL/DDL - Data Definition Language.sql
@@ -1,4 +1,39 @@
 
+## DATABASE ##
+
+CREATE DATABASE nome_do_banco; /* cria um banco de dados */
+SHOW DATABASES; /* mostra os banco de dados */
+USE banco_de_dados; /* seleciona banco de dados */
+DROP DATABASE nome_do_banco; /* apaga um banco de dados */
+
+## TABLE ##
+
+SHOW TABLES; /* mostra as tabelas do banco de dados */
+DESC tabela (ou DESCRIBE); /* mostra detalhes da tabela */
+SHOW COLUMNS FROM tabela; /* mostra colunas */
+SHOW CREATE TABLE tabela; /* mostra script de criação de tabela */
+CREATE TABLE nome_tabela (
+	id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	palavra VARCHAR(100),
+	caractere CHAR(1),
+	texto TEXT,
+	flag BOOLEAN NOT NULL DEFAULT true,
+	data DATE,
+	hora TIME,
+	data_hora DATETIME NOT NULL DEFAULT NOW(),
+	valor DECIMAL(9,2),
+	preco FLOAT(9,2)
+);
+RENAME TABLE tabela TO novo_nome; /* renomeia tabela */
+ALTER TABLE nome_tabela ADD COLUMN nome_coluna tipo(t); /* add coluna na tabela */
+ALTER TABLE nome_tabela CHANGE nome_coluna novo_nome tipo(t); /* renomeia coluna da tabela */
+ALTER TABLE nome_tabela DROP COLUMN nome_coluna; /* remove coluna da tabela */
+ALTER TABLE nome_tabela MODIFY COLUMN nome_coluna tipo()t; /* modifica uma coluna  (MySQL) */
+ALTER TABLE nome_tabela ALTER COLUMN nome_coluna tipo()t; /* modifica uma coluna  (SQL) */
+ALTER TABLE nome_tabela ADD FOREIGN KEY (id) REFERENCES tabela_referencia (id_ref) ON DELETE CASCADE; /* cria chave estrageira, como delete automatico */
+ALTER TABLE nome_tabela DROP FOREIGN KEY (foreign_key_name); /* apaga uma chave estrangeira */
+ALTER TABLE nome_tabela ENGINE=InnoDB /* altera o motor de busca da tabela */
+
 ## tipos de dados ##
 
 # Numéricos
@@ -26,54 +61,13 @@
 
 # link: https://dev.mysql.com/doc/refman/5.7/en/numeric-type-overview.html
 
-## TABLE ##
-
-CREATE TABLE nome_tabela (
-	id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	palavra VARCHAR(100),
-	caractere CHAR(1),
-	texto TEXT,
-	flag BOOLEAN NOT NULL DEFAULT true,
-	data DATE,
-	hora TIME,
-	data_hora DATETIME NOT NULL DEFAULT NOW(),
-	valor DECIMAL(9,2),
-	preco FLOAT(9,2)
-);
-
-RENAME TABLE tabela TO novo_nome; /* renomeia tabela */
-
-ALTER TABLE nome_tabela ADD COLUMN nome_coluna tipo(t); /* add coluna na tabela */
-ALTER TABLE nome_tabela CHANGE nome_coluna novo_nome tipo(t); /* renomeia coluna da tabela */
-ALTER TABLE nome_tabela DROP COLUMN nome_coluna; /* remove coluna da tabela */
-ALTER TABLE nome_tabela MODIFY COLUMN nome_coluna tipo()t; /* modifica uma coluna  (MySQL) */
-ALTER TABLE nome_tabela ALTER COLUMN nome_coluna tipo()t; /* modifica uma coluna  (SQL) */
-ALTER TABLE nome_tabela ADD FOREIGN KEY (id) REFERENCES tabela_referencia (id_ref) ON DELETE CASCADE; /* cria chave estrageira, como delete automatico */
-ALTER TABLE nome_tabela DROP FOREIGN KEY (foreign_key_name); /* apaga uma chave estrangeira */
-ALTER TABLE nome_tabela ENGINE=InnoDB /* altera o motor de busca da tabela */
-
 ## VIEW ##
 
-CREATE VIEW [nome_view] AS SELECT * FROM tabela WHERE .. /* cria uma view */
-
-CREATE OR REPLACE VIEW [nome_view] AS select * from nome_tabela where .. /* edita uma view */
-
-ALTER VIEW [nome_view] AS SELECT * FROM tabela WHERE .. /* edita uma view*/
-
-DROP VIEW [nome_view] /* apaga uma view */
-
-## DATABASE ##
-
-SHOW DATABASES; /* mostra os banco de dados */
-USE banco_de_dados; /* seleciona banco de dados */
-SHOW TABLES; /* mostra as tabelas do banco de dados */
 SHOW FULL TABLES IN nome_do_banco WHERE TABLE_TYPE LIKE 'VIEW'; /* mostra todas as views */
-
-## TABLE ##
-
-DESC tabela (ou DESCRIBE); /* mostra detalhes da tabela */
-SHOW COLUMNS FROM tabela; /* mostra colunas */
-SHOW CREATE TABLE tabela; /* mostra script de criação de tabela */
+CREATE VIEW [nome_view] AS SELECT * FROM tabela WHERE .. /* cria uma view */
+CREATE OR REPLACE VIEW [nome_view] AS select * from nome_tabela where .. /* edita uma view */
+ALTER VIEW [nome_view] AS SELECT * FROM tabela WHERE .. /* edita uma view*/
+DROP VIEW [nome_view] /* apaga uma view */
 
 ## PROCESS ##
 
