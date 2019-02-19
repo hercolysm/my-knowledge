66833c876a62b8b8a7e268e523161f01270aedaf
Hercolys Moraes Araújo <hercolys@fortics.com.br>
2019-02-18 22:51:09 -0300

    Adicionado script de simulação de apostas lotofácil e lotomania

--- linguagens_de_programacao/PHP/apostar_lotofacil.php
+++ linguagens_de_programacao/PHP/apostar_lotofacil.php
@@ -8,14 +8,14 @@ for ($i = 1; $i <= 15; $i++) {
 
 $cont = 0;
 
-while (!$resultados[12]) {
+while (!$resultados[15]) {
 // while ($cont != 100) {
 
 	$cont++;
 
 	$numeros = range(1, 25);
 
-	$apostados = [1,2,3,4,5,8,9,11,12,14,16,18,19,23,24,25];
+	$apostados = [1,2,3,4,5,8,9,11,12,14,16,18,19,23,24];
 	// $apostados = [7,10,13,15,17,20,21,22,25,1,2,3,4,5,6,8,24,19];
 	// $apostados = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18];
 
@@ -26,6 +26,10 @@ while (!$resultados[12]) {
 	// }
 
 	$numeros = range(1, 25);
+
+	shuffle($numeros);
+	shuffle($numeros);
+	shuffle($numeros);
 
 	$sorteados = [];
 
@@ -48,7 +52,7 @@ while (!$resultados[12]) {
 	// print_r($x);
 	// print_r($y);
 
-	$acertos = 16 - count($x);
+	$acertos = 15 - count($x);
 
 	$resultados[$acertos]++;
 
