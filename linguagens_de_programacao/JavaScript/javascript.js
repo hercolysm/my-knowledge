1910e3f922425d94db80df4e729512adc2692a0e
Hercolys Moraes Araújo <hercolysm@gmail.com>
2019-04-25 10:19:21 -0300

    Update javascript.js

--- linguagens_de_programacao/JavaScript/javascript.js
+++ linguagens_de_programacao/JavaScript/javascript.js
@@ -3,3 +3,22 @@
 // cortar string
 var str = "texto";
 var str_cortada = str.substring(inicio, quantidade);
+
+// Informações do browser
+var txt = ""; 
+txt += "<p>Browser CodeName: " + navigator.appCodeName + "</p>";
+txt += "<p>Browser Name: " + navigator.appName + "</p>";
+txt += "<p>Browser Version: " + navigator.appVersion + "</p>";
+txt += "<p>Cookies Enabled: " + navigator.cookieEnabled + "</p>";
+txt += "<p>Browser Language: " + navigator.language + "</p>";
+txt += "<p>Browser Online: " + navigator.onLine + "</p>";
+txt += "<p>Platform: " + navigator.platform + "</p>";
+txt += "<p>User-agent header: " + navigator.userAgent + "</p>";
+alert(txt);
+
+// Verificação se contem
+var reg = new RegExp("Texto procurado", "ig");
+
+if (reg.test("Texto completo")) {
+  alert("Texto procurado foi encontrado");
+}
