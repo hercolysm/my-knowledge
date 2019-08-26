ce771c47323aa6c6650dd1706277c8f736ceab29
Hercolys Moraes Araújo <hercolysm@gmail.com>
2019-08-26 14:44:45 -0300

    Update funcoes.php

--- linguagens_de_programacao/PHP/funcoes.php
+++ linguagens_de_programacao/PHP/funcoes.php
@@ -1,6 +1,13 @@
 /**
  * Recebe uma entrada do teclado
  */
-$nome = readline("Digite o seu nome: ");
+$readline = "Digite o seu nome: \n";
+
+if (PHP_OS == 'WINNT') {
+  echo $readline;
+  $nome = stream_get_line(STDIN, 1024, PHP_EOL);
+} else {
+  $nome = readline($readline);
+}
 
 print "O seu nome é ".$nome;
