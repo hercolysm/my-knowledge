49b9865ccc43113d62b191cdd8d0e632632185c6
Hercolys Moraes Araújo <hercolysm@gmail.com>
2023-01-06 15:40:59 -0300

    Create Funções Nativas.js

---
+++ linguagens_de_programacao/Dynamics 365/JavaScript/Documentação/Funções Nativas.js
@@ -0,0 +1,17 @@
+// Funções Nativas do Dynamics 365
+
+// Pernalização da ação ao clicar no botão de telefone
+if (top.MscrmControls.FieldControls.PhoneNumberControl) {
+
+    top.MscrmControls.FieldControls.PhoneNumberControl.prototype.action = function () {
+        return false;
+    };
+}
+
+// Campo Lookup: Ordem de execução dos eventos
+top.MscrmControls.FieldControls.SimpleLookupControl.prototype.init 
+top.MscrmControls.FieldControls.SimpleLookupControl.prototype.updateView 
+top.MscrmControls.FieldControls.SimpleLookupControl.prototype._updateComponentsManager 
+top.MscrmControls.FieldControls.SimpleLookupControl.prototype._checkUpdatedParameters //(on hover)
+top.MscrmControls.FieldControls.SimpleLookupControl.prototype.getOutputs //(on change)
+top.MscrmControls.FieldControls.SimpleLookupControl.prototype.destroy //(ao clicar no link)
