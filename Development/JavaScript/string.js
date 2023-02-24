1558baff5d9f5842a948caa82ba50e195ab7a160
Hercolys Moraes Araújo <hercolysm@gmail.com>
2023-02-24 13:30:25 -0300

    Create string.js

---
+++ Development/JavaScript/string.js
@@ -0,0 +1,14 @@
+let firstname = 'Homer';
+let lastname = 'Simpson';
+
+// Contat string
+let fullname = firstname + ' ' + lastname;
+
+// Concat Function
+let fullname = firstname.concat(' ', lastname);
+
+// Template String
+let fullname = `${firstname} ${lastname}`;
+
+// Join 
+let fullname = [firstname, lastname].join(' ');
