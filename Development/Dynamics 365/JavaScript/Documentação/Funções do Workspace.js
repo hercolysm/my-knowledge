fb2cdfb7dc5db111f90f58925b2305a26d564eb1
Hercolys Moraes Araújo <hercolysm@gmail.com>
2023-02-24 15:50:25 -0300

    Update Funções do Workspace.js

--- Development/Dynamics 365/JavaScript/Documentação/Funções do Workspace.js
+++ Development/Dynamics 365/JavaScript/Documentação/Funções do Workspace.js
@@ -10,3 +10,37 @@ tab.title = "New Tab Title";
 
 // Desativar o diálogo de encerramento de sessão
 Xrm.Utility.getGlobalContext().saveSettingValue("msdyn_SuppressSessionCloseWarning",true);
+
+// Create tab for edit form 
+let AppTabInput = {
+    templateName: "msdyn_entityrecord",
+    appContext: new Map().set("entityName", "account").set("entityId", "3AAC6936-CC17-EB11-A813-000D3AC1413B"),
+    isFocused: true,
+};
+Microsoft.Apm.createTab(AppTabInput);
+
+/**
+ * Create a new row
+ * App: Centro de administração do Customer Service
+ * Entity: Modelo de Guia de Aplicativo (msdyn_applicationtabtemplate)
+ * Column: Nome Exclusivo (msdyn_uniquename)
+ */
+let templateName = "templateName";
+
+// Create tab for open other website
+let uri = "https://google.com/";
+let AppTabInput = {
+  templateName: templateName,
+  appContext: new Map().set("url", encodeURI(uri)),
+  isFocused: true,
+};
+Microsoft.Apm.createTab(AppTabInput);
+
+// Close tab
+let sessao = Microsoft.Apm.getFocusedSession();
+sessao.getAllTabsForTemplate(templateName).then(function sucess(result) {
+  if (result.length > 0) {
+    let tab = result[0];
+    Microsoft.Apm.closeTab(tab);
+  }
+});
