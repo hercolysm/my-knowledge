144907dffd4be010627538cf464ebf5daa22c87f
Hercolys Moraes Araújo <hercolysm@gmail.com>
2024-02-01 18:05:15 -0300

    Update SampleTestMethods.cs

--- Development/Dynamics 365/Plugin C#/Tests/SampleTestMethods.cs
+++ Development/Dynamics 365/Plugin C#/Tests/SampleTestMethods.cs
@@ -3,15 +3,23 @@ public void TestPlugin()
 {
     ParameterCollection inputParameters = new ParameterCollection();
     inputParameters.Add("Target", new Entity("table_name") { Id = new Guid("00000000-0000-0000-0000-000000000000") });
-
+    
+    EntityImageCollection preImageCollection = new EntityImageCollection();
+    preImageCollection.Add("PreImage", new Entity("table_name") { Id = new Guid("00000000-0000-0000-0000-000000000000") });
+    
+    EntityImageCollection postImageCollection = new EntityImageCollection();
+    postImageCollection.Add("PreImage", new Entity("table_name") { Id = new Guid("00000000-0000-0000-0000-000000000000") });
+    
     XrmRealContext context = new XrmRealContext(service);
     XrmFakedPluginExecutionContext pluginContext = context.GetDefaultPluginContext();
     pluginContext.InputParameters = inputParameters;
+    pluginContext.PreEntityImages = preImageCollection;
+    pluginContext.PostEntityImages = postImageCollection;
     pluginContext.MessageName = "messageName"; // Create | Update | Delete | CustomMessage
     pluginContext.Mode = 1; // 1 - Sync | 0 - Async
     pluginContext.Stage = 40; // 10 - Pre Validation | 20 - Pre Operation | 40 - Post Operation
     pluginContext.UserId = new Guid("{00000000-0000-0000-0000-000000000000}");
     pluginContext.InitiatingUserId = new Guid("{00000000-0000-0000-0000-000000000000}");
-
+    
     context.ExecutePluginWith<PluginName>(pluginContext);
 }
