20ca726de17e8bdc350e1dfb4517028e93186c66
Hercolys Moraes Araujo <hercolysmoraes@live.com>
2024-09-03 19:00:41 -0300

    Added names conversion for SQL Server

--- Databases/SQL/DDL - Data Definition Language.sql
+++ Databases/SQL/DDL - Data Definition Language.sql
@@ -1,10 +1,20 @@
 
 ## DDL - Data Definition Language ##
 
+/*
+Converções de Nomenclatura:
+ Nome do Banco de Dados: PascalCase
+ Nome das Tabelas: PascalCase
+ Nome das Colunas: camelCase
+*/
+
+/* criar banco de dados */
+CREATE DATABASE Banco;
+
 /* cria a tabela produtos */
-CREATE TABLE [dbo].[produtos] (
+CREATE TABLE [dbo].[Produtos] (
     [id]         INT            IDENTITY (3, 2017000) NOT NULL,
-    [cod_barras] NVARCHAR (50)  NOT NULL,
+    [codBarras] NVARCHAR (50)  NOT NULL,
     [nome]       NVARCHAR (100) NOT NULL,
     [marca]      NVARCHAR (50)  NULL,
     [altura]     NVARCHAR (10)  NULL,
@@ -12,37 +22,37 @@ CREATE TABLE [dbo].[produtos] (
     [volume]     NVARCHAR (10)  NULL,
     [custo]      MONEY          DEFAULT ((0)) NOT NULL,
     [valor]      MONEY          DEFAULT ((0)) NOT NULL,
-    CONSTRAINT [PK_produtos] PRIMARY KEY CLUSTERED ([id] ASC)
+    CONSTRAINT [PK_Produtos] PRIMARY KEY CLUSTERED ([id] ASC)
 );
 
 /* renomear coluna */
 USE banco;
 GO
-EXEC sp_rename 'tabela.coluna', 'novo_nome', 'COLUMN';
+EXEC sp_rename 'Produtos.cod_barras', 'codBarras', 'COLUMN';
 GO
 
 /* adicionar coluna */
 #NULL
-ALTER TABLE tabela ADD coluna DATETIME NULL;
+ALTER TABLE Tabela ADD coluna DATETIME NULL;
 GO
 #NOT NULL
-ALTER TABLE tabela ADD coluna DATETIME NOT NULL DEFAULT '2017-01-01 00:00:00';
+ALTER TABLE Tabela ADD coluna DATETIME NOT NULL DEFAULT '2017-01-01 00:00:00';
 GO
 /* OBS: Para o atributo 'NOT NULL' é obrigatório uma definição DEFAULT especificada */
 /* OBS²: Para o tipo 'DATETIME' valor default deve ser uma data válida */
 
 /* alterar coluna */
-ALTER TABLE tabela ALTER COLUMN coluna DATETIME NOT NULL;
+ALTER TABLE Tabela ALTER COLUMN coluna DATETIME NOT NULL;
 GO
 /* OBS: Comando acima falha caso conter registros nulos */
 
 /* excluir coluna */
-ALTER TABLE tabela DROP COLUMN coluna;  
+ALTER TABLE Tabela DROP COLUMN coluna;  
 GO
 
-/* adicionar restrição exclusiva para uma tabela existente */
-ALTER TABLE tabela ADD CONSTRAINT UK__tabela__name UNIQUE (coluna1, ...);
+/* adicionar restrição exclusiva para uma Tabela existente */
+ALTER TABLE Tabela ADD CONSTRAINT UK__Tabela__name UNIQUE (coluna1, ...);
 
 /* excluir uma constraint */
-ALTER TABLE produto DROP CONSTRAINT UQ__produto__B3FE2EFC9AA56808;
+ALTER TABLE Produto DROP CONSTRAINT UQ__Produto__B3FE2EFC9AA56808;
 GO
