29141abe117cbefaffbeccf5067e9a7c3ba25c50
Hercolys Moraes Araujo <avanade.hercolys@grupofleury.com.br>
2025-01-30 18:00:42 -0300

    Added RetrieveMultiple example

---
+++ Development/Dynamics 365/Plugin C#/Exemples/RetrieveMultiple.cs
@@ -0,0 +1,23 @@
+// Define the query
+QueryExpression query = new("account")
+{
+    TopCount = 5
+};
+
+// Add columns to query.ColumnSet
+query.ColumnSet.AddColumns("name", "primarycontactid");
+
+// Add conditions to query.Criteria
+query.Criteria.AddCondition("address1_city", ConditionOperator.Equal, "Redmond");
+
+// Add orders
+query.AddOrder("name", OrderType.Ascending);
+
+// Send the request
+EntityCollection results = service.RetrieveMultiple(query);
+
+// Show the data
+foreach (Entity record in results.Entities)
+{
+    Console.WriteLine($"name:{record.GetAttributeValue<string>("name")}");
+}
