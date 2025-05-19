eee8835b02aefec73ac14282d2e1a387bd026ee8
Hercolys Moraes Araujo <avanade.hercolys@grupofleury.com.br>
2025-05-19 11:12:03 -0300

    Added test function with trace

--- Development/Dynamics 365/Plugin C#/Tests/SampleTestMethods.cs
+++ Development/Dynamics 365/Plugin C#/Tests/SampleTestMethods.cs
@@ -1,5 +1,5 @@
 [TestMethod]
-public void TestPlugin()
+public void TestPluginEntity()
 {
     ParameterCollection inputParameters = new ParameterCollection();
     inputParameters.Add("Target", new Entity("table_name") { Id = new Guid("00000000-0000-0000-0000-000000000000") });
@@ -25,6 +25,33 @@ public void TestPlugin()
 }
 
 [TestMethod]
+public void TestPluginService()
+{
+    var table_name = service.Retrieve(
+        "table_name", new Guid("{00000000-0000-0000-0000-000000000000}"),
+        new ColumnSet("columnname"));
+
+    Entity record = new Entity("record");
+    record["text"] = "Name teste";
+    record["table_ref"] = new EntityReference(table_name.LogicalName, table_name.Id);
+    record["boolean"] = false;
+    service.Create(record);
+
+    ParameterCollection inputParameters = new ParameterCollection();
+    inputParameters.Add("Target", record);
+
+    XrmRealContext context = new XrmRealContext(service);
+    XrmFakedPluginExecutionContext pluginContext = context.GetDefaultPluginContext();
+    pluginContext.InputParameters = inputParameters;
+    pluginContext.MessageName = "Create";
+    pluginContext.Mode = 1; //1 - Sync | 0 - Async
+    pluginContext.Stage = 20; //40 - Post Operation, 20 - Pre Operation, 10 - Pre Validation
+    pluginContext.UserId = Guid.Parse("{00000000-0000-0000-0000-000000000000}");
+
+    context.ExecutePluginWith<EnvioPluginName>(pluginContext);
+}
+
+[TestMethod]
 [DataRow("value 1", "value 2")]
 public void TestPluginWithVar(string var1, string var2)
 {
@@ -41,3 +68,14 @@ public void TestPluginWithVar(string var1, string var2)
         Assert.Fail(ex.Message);
     }
 }
+
+[TestMethod]
+public void TestFunctionWithTrace()
+{
+    XrmRealContext context = new XrmRealContext(service);
+    var trace = context.GetFakeTracingService();
+
+    ClassName _object = new ClassName(service);
+    var agendamentos = _object.FunctionName(service, trace, "var 1");
+}
+
