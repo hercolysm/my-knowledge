51e7412e11338fa42ec0df4ef070ccd0dd51d02f
Hercolys Moraes Araujo <avanade.hercolys@grupofleury.com.br>
2025-12-11 17:17:50 -0300

    Added PowerShell script for Power Apps tests

---
+++ Development/Dynamics 365/Tools/Windows PowerShell ISE.ps1
@@ -0,0 +1,16 @@
+﻿Install-Module -Name Microsoft.PowerApps.Administration.PowerShell -Scope CurrentUser
+Install-Module -Name Microsoft.PowerApps.PowerShell -AllowClobber -Scope CurrentUser
+
+# Sign in (tenant-wide)
+Add-PowerAppsAccount
+ 
+# If you already know only the App ID:
+Get-AdminPowerApp -AppId "c647e1a9-c393-9988-1731-9c9848c7b1e1" | Format-List DisplayName, AppId, EnvironmentName, Owner, CreatedTime
+ 
+# Enumerate all environments, then search for the App ID:
+Get-AdminPowerApp | Where-Object { $_.AppId -eq "c647e1a9-c393-9988-1731-9c9848c7b1e1" } |
+  Select-Object DisplayName, AppId, EnvironmentName
+ 
+# If you suspect a Dataverse for Teams environment:
+Get-AdminPowerAppEnvironment -EnvironmentName "<teams-env-id>" |
+  Get-AdminPowerApp | Where-Object { $_.AppId -eq "c647e1a9-c393-9988-1731-9c9848c7b1e1" }
